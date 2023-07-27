5bffef2207b336a5da330d47ac9baee21f35099f
Tarif <tarifuddinrajee@gmail.com>
2023-07-27 21:39:14 +0600

    onk kiso add korsi

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: ["./src/**/*.{html,js}"],
-    theme: {
-      extend: {},
+    daisyui: {
+      themes: false,
     },
-    plugins: [],
+    plugins: [require("daisyui")],
+
   }
